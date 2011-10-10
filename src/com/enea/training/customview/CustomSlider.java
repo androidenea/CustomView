07e7244fe1f5d7feb6284e36e5363272ba636bd4
Robert Niemi <robert.niemi@enea.com>
2011-10-10 12:48:39 +0200

    Step 2.3 onDraw
    Implemented drawing method for slider. This includes adding and initialising a few member variables.

--- src/com/enea/training/customview/CustomSlider.java
+++ src/com/enea/training/customview/CustomSlider.java
@@ -2,6 +2,8 @@ package com.enea.training.customview;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,6 +12,13 @@ public class CustomSlider extends View {
 
   private final Drawable               mIndicator;
   private final Drawable               mBackground;
+  private float                        mMin;
+  private float                        mMax;
+  private float                        mPosition;
+  private Rect                         mViewRect;
+  private int                          mIndicatorOffset;
+  private int                          mIndicatorMaxPos;
+  private int                          mIndicatorMinPos;
 
   public CustomSlider(final Context context) {
     this(context, null, 0);
@@ -26,5 +35,39 @@ public class CustomSlider extends View {
     final Resources res = context.getResources();
     mIndicator = res.getDrawable(R.drawable.indicator_horizontal);
     mBackground = res.getDrawable(R.drawable.background_horizontal);
+
+    mMin = -1.0f;
+    mMax = 1.0f;
+    mPosition = (mMax - mMin) / 2 + mMin;
+  }
+
+  @Override
+  protected void onDraw(final Canvas canvas) {
+    if (mViewRect == null) {
+      mViewRect = new Rect();
+      getDrawingRect(mViewRect);
+      mIndicatorOffset = mIndicator.getIntrinsicWidth();
+      mIndicatorMaxPos = mViewRect.right - mIndicatorOffset;
+      mIndicatorMinPos = mViewRect.left + mIndicatorOffset;
+      mBackground.setBounds(mViewRect.left, mViewRect.top, mViewRect.right,
+          mViewRect.bottom);
+    }
+
+    final float pos;
+    final int left;
+    final int right;
+    final int top;
+    final int bottom;
+    pos = mIndicatorMinPos
+        + ((mIndicatorMaxPos - mIndicatorMinPos) / (mMax - mMin))
+        * (mPosition - mMin);
+    left = (int) pos - (mIndicator.getIntrinsicWidth() / 2);
+    top = mViewRect.centerY() - (mIndicator.getIntrinsicHeight() / 2);
+    right = left + mIndicator.getIntrinsicWidth();
+    bottom = top + mIndicator.getIntrinsicHeight();
+    mIndicator.setBounds(left, top, right, bottom);
+
+    mBackground.draw(canvas);
+    mIndicator.draw(canvas);
   }
 }
