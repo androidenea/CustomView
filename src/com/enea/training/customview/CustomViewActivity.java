414d5c2a9b9301030b7990f18b50033ac252b751
Robert Niemi <robert.niemi@enea.com>
2011-10-10 10:37:11 +0200

    Step 1. Basic activity.
    One simple activity that will be used as test harness.
    Contains one SeekBar view, but that will soon be replaced by the custom view.

---
+++ src/com/enea/training/customview/CustomViewActivity.java
@@ -0,0 +1,12 @@
+package com.enea.training.customview;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CustomViewActivity extends Activity {
+  @Override
+  public void onCreate(final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
